880a10cf64c4677ee0c6aa1eacb4836e5b058f9e
Alexander <chandyalex@outlook.com>
2018-02-11 21:11:42 -0600

    Fix about one child component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 class About extends Component {
   render () {
     return (
-      <p>Project using ReactJS, Axios & Webpack for load images of dogs by raze from DogCEO</p>
-      <label>By chandyalexcr</label>
-
+      <div>
+        <p>Project using ReactJS, Axios & Webpack for load images of dogs by raze from DogCEO</p>
+        <label>By chandyalexcr</label>
+      </div>
     )
   }
 }
